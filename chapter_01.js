415a2b05cd46c97c2349174211b2abdcb7330dbe
fizzajawed85 <angelfizza85@gmail.com>
2025-04-04 00:28:56 +0500

    Completed

---
+++ chapter_01.js
@@ -0,0 +1,25 @@
+/* Chapter_01 ALERT */
+
+// 1. Write a script to greet your website visitor using JS alert box.
+
+alert("Hello! Welcome to my JavaScript assignment");
+
+// 2. Write a script to display following message on your web page:
+
+alert("Error! Please enter a valid password.");
+
+// 3. Write a script to display following message on your web page: (Hint : Use line break)
+
+alert("Welcome to JS Code\nHappy Coding!");
+
+// 4. Write a script to display following messages in sequence:
+
+alert("Welcome to JS Land"); 
+alert("Happy Coding!\nPrevent this page from creating additional dialogs"); 
+
+// 5. Generate the following message through browser’s developer console:
+
+console.log("Hello.. I can run JS through my web browser's console");
+
+
+
---
+++ chapter_03.js
@@ -0,0 +1,28 @@
+/* Chapter_03 VARIABLES FOR NUMBERS */
+
+// 1. Declare a variable called age & assign to it your age. Show your age in an alert box.
+
+var age = 24;
+alert(age);
+
+// 2. Declare & initialize a variable to keep track of how many times a visitor has visited a web page. Show his/her number of visits on your web page. For example: “You have visited this site N times”.
+
+var visitCount = 14;
+alert(`You have visited this site ${visitCount} times.`);
+
+// 3. Declare a variable called birthYear & assign to it your birth year. Show the following message in your browser:
+
+var birthYear = 1990;
+alert(`My birth year is ${birthYear}\nData type of my declared variable is number`);
+
+// 4. A visitor visits an online clothing store www.xyzClothing.com . Write a script to store in variables the following information:
+// a. Visitor’s name
+// b. Product title
+// c. Quantity i.e. how many products a visitor wants to order
+// Show the following message in your browser: “John Doe ordered 5 T-shirt(s) on XYZ Clothing store”.
+
+var visitorName = "John Doe";
+var productTitle = "T-shirt";
+var quantity = 5;
+
+alert(`${visitorName} ordered ${quantity} ${productTitle}(s) on XYZ Clothing store.`);
