653422b55ce49bf519978353e4bd4af95ad87768
fizzajawed85 <angelfizza85@gmail.com>
2025-04-11 23:55:20 +0500

    Assignment_19 Completed

---
+++ chapters_1-3.js
@@ -0,0 +1,102 @@
+// Assignment - 19 
+
+// Chapter 1 (Alerts)
+
+
+/* 1. Alert these following (individually):
+• First Name
+• Last Name
+• Email
+• Phone Number
+• Password */
+
+alert("Fizza");
+alert("jawed");
+alert("fizzajawed012@gmail.com");
+alert("01234567890");
+alert("xyz@123#");
+
+/* 2. Correct this statement: alert"You're learning JavaScript!"; */
+
+alert("You're learning JavaScript!");
+
+/* 3. Code an alert statement displaying any message you like. */
+
+alert("Welcome to the world of JavaScript!");
+
+
+// Chapter 2 (Variables for string)
+
+
+
+/* 1. Declare any variable in the camelCase format. */
+
+var studentName ;
+
+/* 2. Declare a variable of your choice without defining it. Then, in a second statement, assign it a string of your choice. */
+
+var favoriteFood;
+favoriteFood = "Pizza";
+console.log(favoriteFood);
+
+
+/* 3. Declare the variable teamName and Alert your Team name. */
+
+var teamName = "Code Warriors";
+alert(teamName);
+
+
+/* 4. This statement has already been coded. var bestMan = "Charlie"; Assign the variable a new string. */
+
+var bestMan = "Charlie";
+bestMan = "Adam";
+console.log(bestMan);
+
+// Chapter 3 (Variables for numbers)
+
+
+/* 1. Declare a variable “caseQty” */
+
+var caseQty;
+
+
+/* 2. Assign to the variable caseQty, which has already been declared, the value 144. */
+
+caseQty = 144;
+
+
+/* 3. Rewrite this statement so the variable can be used in a math operation. var num = "9"; */
+
+var num = "9";
+
+var num = 9;
+
+
+/* 4. In one statement declare a variable. In a second statement assign it the sum of 2 numbers. */
+
+var sum;
+sum = 7 + 8;
+console.log(sum);
+
+var num1 = 1;
+var num2 = 4;
+var sum = num1 + num2 ;
+console.log(sum);
+
+/* 5. What is the value of orderTotal?
+      var merchTotal = 100;
+      var shippingCharge = 10;
+      var orderTotal = merchTotal + shippingCharge;
+      Try it yourself. */
+
+var merchTotal = 100;
+var shippingCharge = 10;
+var orderTotal = merchTotal + shippingCharge;
+console.log(orderTotal);
+
+/* 6. In the first statement declare a variable and assign it a number. In the second statement, change the value of the variable by adding it together with a number. */
+
+var value = 9;  
+value = value + 6;  
+console.log(value);
+
